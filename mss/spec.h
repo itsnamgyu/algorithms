36e3ce9d6d9a631bfbc75dfb20fc70ca369ca3b3
Namgyu Ho <itsnamgyu@gmail.com>
2018-09-20 00:33:41 +0000

    definitions of mss solve function + result struct

---
+++ mss/spec.h
@@ -0,0 +1,16 @@
+typedef struct _mss_result {
+	char filename[100];
+	int algo_index;
+	int input_size;
+	int index_a;
+	int index_b;
+	int sum;
+	int running_time;
+} mss_result;
+
+/*
+ * A function that takes inputs for the mss problem, and returns
+ * a **dynamically allocated** mss_result containing the answers.
+ * Returns NULL on invalid input.
+ */
+typedef mss_result *mss_function(int *data, int size);
