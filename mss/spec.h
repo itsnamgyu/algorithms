af3cfc72608bcd401453ebf312f4e38ec811f09c
Namgyu Ho <itsnamgyu@gmail.com>
2018-09-21 06:39:01 +0000

    return mss result as value
    not big enough to be returned as dynamically allocated
    reference
    
    (should have committed before this)

--- mss/spec.h
+++ mss/spec.h
@@ -13,9 +13,8 @@ typedef struct _mss_result {
 
 /*
  * A function that takes inputs for the mss problem, and returns
- * a **dynamically allocated** mss_result containing the answers.
- * Returns NULL on invalid input.
+ * an mss_result containing the answers. Returns NULL on invalid input.
  */
-typedef mss_result *mss_function(int *data, int size);
+typedef mss_result mss_function(int *data, int size);
 
 #define DEBUG_EXPR "line %d of \"%s\" function <%s>\n",__LINE__,__FILE__,__func__
