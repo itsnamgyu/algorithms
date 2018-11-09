e11c7766333d68548b4f687a534f18bd010e4cbc
Namgyu Ho <itsnamgyu@gmail.com>
2018-11-09 16:31:48 +0900

    add insertion_sort to header

--- sort/include/sorter.hpp
+++ sort/include/sorter.hpp
@@ -8,6 +8,7 @@ bool validate_sort(sort_func sort, bool verbose=false, int iterations=5000);
 
 void std_sort(Data &data);
 void null_sort(Data &data);
+void insertion_sort(Data &data);
 void merge_sort(Data &data);
 void quick_sort(Data &data);
 void ho_sort(Data &data);
