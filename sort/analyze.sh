2af06b23c1cb8dde3404dd5651adce4d60b663d2
itsnamgyu <itsnamgyu@gmail.com>
2018-11-14 04:16:59 -0800

    add notification to background analysis script

--- sort/analyze.sh
+++ sort/analyze.sh
@@ -1,1 +1,7 @@
 tmux new-session -s 'algo_analysis' -d 'make analyze_all | tee analysis_log'
+
+# personal notification command
+command -v ho &>/dev/null
+if [[ $? == 0 ]]; then
+	ho "algo analysis complete"
+fi
