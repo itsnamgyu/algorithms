6bc6c259e3abce5978e9e67d6b8a44ea158aa69d
itsnamgyu <itsnamgyu@gmail.com>
2018-11-14 04:33:04 -0800

    finalize background analysis
    + apparently, you can't tee output from execution in Makefile?

--- sort/run.sh
+++ sort/run.sh
@@ -1,7 +1,7 @@
-tmux new-session -s 'algo_analysis' -d 'make analyze_all | tee log.txt; exit'
-
-# personal notification command
+# ho: personal notification script
+tmux new-session -s 'algo' -d '
+make analyze_all;
 command -v ho &>/dev/null
 if [[ $? == 0 ]]; then
 	ho "algo analysis complete"
-fi
+fi'
