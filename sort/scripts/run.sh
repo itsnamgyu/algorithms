66fc9c0d5b0320ed437ab676c4dc5b8defdb2ef4
itsnamgyu <itsnamgyu@gmail.com>
2018-11-16 05:41:12 -0800

    fix notification script

--- sort/scripts/run.sh
+++ sort/scripts/run.sh
@@ -5,4 +5,4 @@ make analyze
 ./analyze
 
 # personal notification
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":"algo analysis complete"}" https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"algo analysis complete\"}" https://hooks.slack.com/services/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
--- sort/scripts/run_all.sh
+++ sort/scripts/run_all.sh
@@ -5,4 +5,4 @@ make analyze_all
 ./analyze_all
 
 # personal notification
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":"algo analysis complete"}" https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"algo analysis complete\"}" https://hooks.slack.com/services/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
