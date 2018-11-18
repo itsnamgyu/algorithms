e4950a59afde8235943c3526db6d369969ab1bda
Namgyu Ho <itsnamgyu@gmail.com>
2018-11-18 22:30:39 +0900

    implement and test bucket_sort

--- sort/src/bucket_sort.cpp
+++ sort/src/bucket_sort.cpp
@@ -4,32 +4,69 @@
 #include <cassert>
 #include <vector>
 
-int BUCKET_SIZE = 10;
+int BUCKET_EXP = 4;  // size = 2^exp;
 
-void _bucket_sort(int *array, int begin, int end) {
-}
+
+static int get_max(int *array, int begin, int end);
+
 void bucket_sort(int *array, int begin, int end) {
-	if (end - begin < (1 << 10)) {
-		insertion_sort(array, begin, end);
-		return;
-	} else if (end - begin < (1 << 15)) {
-		heap_sort(array, begin, end);
-		return;
+	unsigned int max = get_max(array, begin, end) - INT_MIN;
+	unsigned int bucket_size = 1 << BUCKET_EXP;
+
+	auto *buckets0 = new std::vector<unsigned int>[bucket_size];
+	auto *buckets1 = new std::vector<unsigned int>[bucket_size];
+	for (int i = 0; i < bucket_size; ++i) {
+		buckets0[i] = std::vector<unsigned int>();
+		buckets1[i] = std::vector<unsigned int>();
+		buckets0[i].reserve((end - begin) / bucket_size);
+		buckets1[i].reserve((end - begin) / bucket_size);
 	}
 
-	auto *_buckets = new std::vector<int>((long long) INT_MAX - INT_MIN + 1, 0);
-	int *buckets = _buckets->data() - INT_MIN;
+	std::vector<unsigned int> *src = NULL;
+	std::vector<unsigned int> *dst = NULL;
+
+	unsigned int mask = (1 << BUCKET_EXP) - 1;
+
+	for (unsigned int shift = 0; shift < 32 && (shift == 0 || max >> shift); shift += BUCKET_EXP) {
+		if (shift == 0) {
+			for (int i = begin; i <= end; ++i) {
+				unsigned int value = array[i] - INT_MIN;
+				buckets0[value & mask].push_back(value);
+			}
+			src = buckets0;
+			dst = buckets1;
+		} else {
+			unsigned int shifted_mask = mask << shift;
+			for (int i = 0; i < bucket_size; ++i) {
+				for (auto const& value : src[i])
+					dst[(value & shifted_mask) >> shift].push_back(value);
+				src[i].clear();
+			}
+
+			auto *tmp = src;
+			src = dst;
+			dst = tmp;
+		}
+	}
+
+	int index = begin;
+	for (int i = 0; i < bucket_size; ++i) {
+		for (auto const& value : src[i])
+			array[index++] = value + INT_MIN;
+	}
 	
-	for (int i = begin; i <= end; ++i)
-		++buckets[array[i]];
-
-	int index = 0;
-	for (long long i = INT_MIN; i <= INT_MAX; ++i)
-		while (buckets[i]--)
-			array[index++] = i;
-
-	delete _buckets;
+	delete[] buckets0;
+	delete[] buckets1;
 }
+
+static int get_max(int *array, int begin, int end) {
+	int max = INT_MIN;
+	for (int i = begin; i <= end; ++i)
+		if (array[i] > max) max = array[i];
+	return max;
+}
+
+
 
 #ifdef TEST
 #undef TEST
