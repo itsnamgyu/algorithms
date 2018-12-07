aacefc1b15bd0992c1406f49bf4c3fffa0aeb25b
itsnamgyu <itsnamgyu@gmail.com>
2018-12-07 03:10:29 -0800

    add interface for data related classes

---
+++ huffman/include/data.hpp
@@ -0,0 +1,66 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <queue>
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+class Data {
+  public:
+    /*
+     * Basic constructors and destructors
+     */
+    Data();
+    Data(const uchar *data, int n);
+    ~Data();
+
+    /*
+     * Read n bytes from the specified stream. If n is set to
+     * -1, read until the end fo the file.
+     */
+    static Data from_stream(FILE *stream, int n=-1);
+
+    /*
+     * Basic comparison functions for testing etc.
+     */
+    bool operator==(const Data& other) const;
+
+  // private:  // maybe in a perfect world
+    std::vector<uchar> data;
+
+    /*
+     * For our compressed files, we will be dealing with individual bits.
+     * Hence, the last few bits of the last byte written to the file may
+     * not be used. We need to keep track of this.
+     */
+    int unused_tail_bits;
+}
+
+class BitSequence {
+  public:
+    /*
+     * Basic constructors and destructors
+     */
+    BitSequence();
+    ~BitSequence();
+
+    /*
+     * Pack the individual bits in the BitSequence into a string of
+     * unsigned chars (wrapped in a Data object).
+     */
+    Data compile() const;
+
+    /*
+     * Basic comparison functions for testing etc.
+     */
+    bool operator==(const BitSequence& other) const;
+    
+    /*
+     * Append the bit contents of tail.
+     */
+    void append(const BitSequence& tail);
+
+  // private:  // maybe in a perfect world
+    std::vector<bool> data;
+}
